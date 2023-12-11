66751ca29dc19a170d5a818cff55e8990a6f8923
sedraxnet <sedraxnet@gmail.com>
2023-12-11 15:09:33 +0100

    Update params.go

--- domain/dagconfig/params.go
+++ domain/dagconfig/params.go
@@ -215,23 +215,7 @@ var MainnetParams = Params{
 	DefaultPort: "16111",
 	DNSSeeds: []string{
 		// This DNS seeder is run by Wolfie
-		"mainnet-dnsseed.kas.pa",
-		// This DNS seeder is run by Denis Mashkevich
-		"mainnet-dnsseed-1.kaspanet.org",
-		// This DNS seeder is run by Denis Mashkevich
-		"mainnet-dnsseed-2.kaspanet.org",
-		// This DNS seeder is run by Constantine Bytensky
-		"dnsseed.cbytensky.org",
-		// This DNS seeder is run by Georges Künzli
-		"seeder1.kaspad.net",
-		// This DNS seeder is run by Georges Künzli
-		"seeder2.kaspad.net",
-		// This DNS seeder is run by Georges Künzli
-		"seeder3.kaspad.net",
-		// This DNS seeder is run by Georges Künzli
-		"seeder4.kaspad.net",
-		// This DNS seeder is run by Tim
-		"kaspadns.kaspacalc.net",
+		"ec2-65-2-9-66.ap-south-1.compute.amazonaws.com",
 	},
 
 	// DAG parameters
@@ -298,9 +282,7 @@ var TestnetParams = Params{
 	RPCPort:     "16210",
 	DefaultPort: "16211",
 	DNSSeeds: []string{
-		"testnet-10-dnsseed.kas.pa",
-		// This DNS seeder is run by Tiram
-		"seeder1-testnet.kaspad.net",
+		"ec2-65-2-9-66.ap-south-1.compute.amazonaws.com",
 	},
 
 	// DAG parameters
